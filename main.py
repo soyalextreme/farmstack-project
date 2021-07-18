6e0ad48a3ec26d337ed7db5688bd7247a8bf8e36
soyalextreme <soyalextreme@gmail.com>
2021-07-18 14:29:58 -0500

    Init server

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware # cors origin share => frontend is on a diferent origin
+
+# App object
+app = FastAPI()
+
+
+origins = ['https://localhost:3000']
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {
+        "ping": "pong"
+    }
+
+
+@app.get("api/todo")
+async def get_todo():
+    return 1
+
+
+@app.get("api/todo{id}")
+async def get_todo_by_id(id):
+    return 1
+
+
+@app.post("api/todo")
+async def post_todot(todo):
+    return 1
+
+
+@app.put("api/todo{id}")
+async def put_todo(id, data):
+    return 1
+
+
+@app.delete("api/todo{id}")
+async def delete_todo(id):
+    return 1
